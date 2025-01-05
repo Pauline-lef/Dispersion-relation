7d48f3bd0ee842ce1bd23b38836ea88f942b7534
Pauline-lef <lefe0362@gmail.com>
2025-01-05 13:06:13 +0100

    Update Dirac_perturbation.py

--- Dirac_perturbation.py
+++ Dirac_perturbation.py
@@ -32,7 +32,7 @@ data = pd.read_csv(fichier_path, delim_whitespace=True, header=None)
 # Convertion to numpy array
 L = 0.4  # m 
 l = 0.04 # m
-T = data[0][len(data[0])-1] * 30/25 # ! problème de dt au niveau du fichier
+T = data[0][len(data[0])-1] * 30/25 # The data were created using a resolution of 30fps.
 N_x = max(data[1])+1 
 N_t = int(len(data[0])/N_x)
     
